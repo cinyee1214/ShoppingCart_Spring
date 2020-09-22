6244a80e9d2e44f6f3ecacd622bd11cb6bd1a591
cinyee1214 <xinyizhao1214@gmail.com>
2020-09-21 22:21:07 -0400

    Spring Security/Spring MVC implementation

---
+++ src/main/java/onlineShop/service/CustomerService.java
@@ -0,0 +1,28 @@
+package onlineShop.service;
+
+import onlineShop.model.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import onlineShop.dao.CustomerDao;
+import onlineShop.model.Customer;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    public void addCustomer(Customer customer) {
+        customer.getUser().setEnabled(true);
+
+        Cart cart = new Cart();
+        customer.setCart(cart);
+
+        customerDao.addCustomer(customer);
+    }
+
+    public Customer getCustomerByUserName(String userName) {
+        return customerDao.getCustomerByUserName(userName);
+    }
+}
