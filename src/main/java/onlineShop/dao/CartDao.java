c13b9e3a8b6cb158745a0bb9e6ba48bef297b8a2
cinyee1214 <xinyizhao1214@gmail.com>
2020-09-26 12:14:00 -0400

    cartItem and cart, CRUD

---
+++ src/main/java/onlineShop/dao/CartDao.java
@@ -0,0 +1,28 @@
+package onlineShop.dao;
+
+import java.io.IOException;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import onlineShop.model.Cart;
+import onlineShop.model.CartItem;
+
+@Repository
+public class CartDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public Cart getCartById(int cartId) {
+        Cart cart = null;
+        try (Session session = sessionFactory.openSession()) {
+            cart = session.get(Cart.class, cartId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cart;
+    }
+}
