928a85701139b2375d6e84a88e1a1a8aaed716af
cinyee1214 <xinyizhao1214@gmail.com>
2020-09-28 22:06:47 -0400

    Spring Web Flow

--- src/main/java/onlineShop/dao/CartDao.java
+++ src/main/java/onlineShop/dao/CartDao.java
@@ -1,3 +1,4 @@
+
 package onlineShop.dao;
 
 import java.io.IOException;
@@ -19,10 +20,45 @@ public class CartDao {
     public Cart getCartById(int cartId) {
         Cart cart = null;
         try (Session session = sessionFactory.openSession()) {
-            cart = session.get(Cart.class, cartId);
+            session.beginTransaction();
+            cart = (Cart) session.get(Cart.class, cartId);
+            session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return cart;
     }
+
+    public Cart validate(int cartId) throws IOException {
+        Cart cart = getCartById(cartId);
+        if (cart == null || cart.getCartItem().size() == 0) {
+            throw new IOException(cartId + "");
+        }
+        update(cart);
+        return cart;
+    }
+
+    private void update(Cart cart) {
+        double total = getSalesOrderTotal(cart);
+        cart.setTotalPrice(total);
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.saveOrUpdate(cart);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private double getSalesOrderTotal(Cart cart) {
+        double total = 0;
+        List<CartItem> cartItems = cart.getCartItem();
+
+        for (CartItem item : cartItems) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
 }
